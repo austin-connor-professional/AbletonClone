60cd9e00721a90632f18aca9a4859b0ccb17ee45
nfb22139 <austinjconnor@gmail.com>
2023-12-15 20:52:30 +0000

    Added new body images

--- ableton-clone/src/components/mainBody/mainBody.jsx
+++ ableton-clone/src/components/mainBody/mainBody.jsx
@@ -5,6 +5,9 @@ import '../../App.css';
 import BeatMixerImage from '../../assets/image2.png';
 import BlurredWalk from '../../assets/iamge3.png';
 import VideoImage from '../../assets/videoImage.png';
+import WhiteBoard from '../../assets/image4.png';
+import GroupSession from '../../assets/image5.png';
+import AbletonSign from '../../assets/image6.png';
 
 const MainBody = () => {
     return (
@@ -22,7 +25,14 @@ const MainBody = () => {
             <div className="video-image-container"><img className="video-image" src={VideoImage} alt="image of a video"/>
             <p className="info-about-image regular-text">Random info</p></div>
             <div className="body-text regular-text">
-                <h3 className="regular-text">We believe it takes focus to create truly oustanding instruments. We only work on a few products and we strive to make them great.</h3>
+                <h3 className="regular-text">We believe it takes focus to create truly oustanding instruments. We only work on a few products and we strive to make them great.gi</h3>
+            </div>
+            <div className="green-container">
+                <div className="green-box">
+                    <img src={WhiteBoard} alt="image of someone using a whiteboard"></img>
+                    <img src={AbletonSign} alt="image of ableton sign on side of building"></img>
+                </div>
+                <img src={GroupSession} alt="image of two people collaborating as a group" className="collab-image"></img>
             </div>
         </div>
     );
